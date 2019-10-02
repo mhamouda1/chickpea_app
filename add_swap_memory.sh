2205991c2edc80a7bbbba5623d2e0967f0d96d50
Your Name <you@example.com>
2019-10-02 18:42:42 +0000

    cleanup

--- add_swap_memory.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-sudo dd if=/dev/zero of=/swapfile bs=2048 count=2097152 && \
-sudo chmod 600 /swapfile && \
-sudo mkswap /swapfile && \
-echo '/swapfile            swap swap    0   0' >> /etc/fstab && \
-sudo mount -a && \
-sudo swapon -s && \
-sudo swapon /swapfile && \
-free -h
