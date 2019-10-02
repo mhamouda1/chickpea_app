ef69582cc0673c1a455b06f69c56a193620fd034
Your Name <you@example.com>
2019-10-02 21:02:27 +0000

    wip

--- get_chickpea.rb
+++ get_chickpea.rb
@@ -24,7 +24,7 @@ require 'open3'
 @github_repo = "mhamouda1/chickpea-app"
 @recommended_linux = "CentOS Linux release 7.6.1810"
 @installations = ["tmux", "vim", "ag", "docker", "docker-compose"]
-@scripts_dir = "lib"
+@scripts_dir = "scripts"
 
 ####################################
 ######### --- AWS Variables --- ####
