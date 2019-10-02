73ab78753a5620d79907e678b401640931683ec4
Your Name <you@example.com>
2019-10-02 23:59:50 +0000

    aliases

--- setup.rb
+++ setup.rb
@@ -15,7 +15,7 @@ require 'open3'
 ######### --- Configuration --- ####
 ####################################
 @debug = false
-@configure_aws = true
+@configure_aws = false
 @create_terraform_variables = true
 @kill_all_ruby_processes = false
 @add_swap_memory = true
@@ -84,12 +84,14 @@ end
   tfa: "terraform apply -auto-approve",
   tfd: "terraform destroy -force",
   wip: "git add . && git commit -m \'wip\' && git push",
+  gs: "git status",
+  gaa: "git add .",
   k: "kubectl",
   d: "docker",
   dc: "docker-compose",
   sbp: "source ~/.bash_profile",
   cbp: "cat ~/.bash_profile",
-  ebp: "vim ~/.bash_profile"
+  ebp: "vim ~/.bash_profile",
 }
 @linux_aliases.each do |k,v|
   `echo 'alias #{k}="#{v}"' >> ~/.bash_profile` if `cat ~/.bash_profile | grep 'alias #{k}='`.empty?
