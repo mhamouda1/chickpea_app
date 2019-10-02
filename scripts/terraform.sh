f8c93980513a958b32943de87cdd9a22b2db6fb5
Your Name <you@example.com>
2019-10-02 21:25:40 +0000

    adds terraform

--- scripts/terraform.sh
+++ scripts/terraform.sh
@@ -1,3 +1,6 @@
 yum install wget unzip -y && \
 wget https://releases.hashicorp.com/terraform/0.12.9/terraform_0.12.9_linux_amd64.zip && \
-unzip ./terraform_0.12.9_linux_amd64.zip –d /usr/local/bin
+mv terraform_0.12.9_linux_amd64.zip /usr/local/bin && \
+cd /usr/local/bin && \
+unzip terraform_0.12.9_linux_amd64.zip && \
+cd -
