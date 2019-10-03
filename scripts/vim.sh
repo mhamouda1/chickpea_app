9635426021d6b9313fab00eebefaa9d18c4e863b
mhamouda1 <55758648+mhamouda1@users.noreply.github.com>
2019-10-02 18:33:51 -0700

    Update vim.sh

--- scripts/vim.sh
+++ scripts/vim.sh
@@ -7,5 +7,6 @@ echo ':imap jj <Esc>' >> ~/.dotfiles/vimrc && \
 echo ':imap jk <Esc>'  >> ~/.dotfiles/vimrc && \
 echo ':imap kj <Esc>'  >> ~/.dotfiles/vimrc && \
 echo ':nmap Z :wa<CR>'  >> ~/.dotfiles/vimrc && \
+echo 'autocmd BufNewFile,BufRead *.tf set syntax=ruby'  >> ~/.dotfiles/vimrc && \
 echo "Bundle 'ervandew/screen'" >> ~/.custom.vim-plugins && \
 ./setup.sh
