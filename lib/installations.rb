825739722acb1526e9a45cc32906f2406844dcf6
Your Name <you@example.com>
2019-10-03 00:10:33 +0000

    wip

--- lib/installations.rb
+++ lib/installations.rb
@@ -1,7 +1,7 @@
 if @add_swap_memory
-  IO.popen("bash ./#{@scripts_dir}/add_swap_memory.sh") { |io| while (line = io.gets) do puts line end } if `cat /etc/fstab | grep swapfile`.empty?
+  IO.popen("bash #{@scripts_dir}/add_swap_memory.sh") { |io| while (line = io.gets) do puts line end } if `cat /etc/fstab | grep swapfile`.empty?
 end
 
 @installations.each do |installation|
-  IO.popen("bash ./#{@scripts_dir}/#{installation}.sh") { |io| while (line = io.gets) do puts line end } if `which #{installation} 2>&1` =~ /no #{installation}/
+  IO.popen("bash #{@scripts_dir}/#{installation}.sh") { |io| while (line = io.gets) do puts line end } if `which #{installation} 2>&1` =~ /no #{installation}/
 end
