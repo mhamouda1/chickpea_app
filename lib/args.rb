3918b053b3be9ee5b87edb073903534842853927
Your Name <you@example.com>
2019-10-06 15:36:01 +0000

    write docker file env

--- lib/args.rb
+++ lib/args.rb
@@ -1,7 +1,8 @@
 #get arguments
 ARGV.each do |arg|
   @argument = arg.dup
-  @argument[0..1] = "" #remove dashes
+  @argument[0] = "" if @argument.include?("-")
+  @argument[0..1] = "" if @argument.include?("--")
   field, value = @argument.split("=")
   instance_variable_set('@' + field, value)
 end
--- lib/aws_variables.rb
+++ lib/aws_variables.rb
@@ -12,4 +12,11 @@ if @configure_aws
     `echo "export #{k.to_s}=#{@aws_vars[k]}" >> ~/.bash_profile`
     `echo 'export TF_VAR_#{k.to_s}=\$#{k.to_s}' >> ~/.bash_profile` if @create_terraform_variables
   end
+
+  #replace docker/development.env file with correct account number
+  docker_development_env_path = "../chickpea-app/docker/web/development.env"
+  new_docker_development_env = `cat #{docker_development_env_path}`.gsub(/AWS_ACCOUNT_NUMBER=\d+/,
+                                                                         `source ~/.bash_profile && echo AWS_ACCOUNT_NUMBER=$AWS_ACCOUNT_NUMBER`.chomp)
+  `echo '#{new_docker_development_env.chomp}' > #{docker_development_env_path}`
 end
+
